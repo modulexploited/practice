99bd228b9274be8e26c1f288949d500ba4990d81
ModuleXploited <harsha@modulexploited.org>
2014-09-09 20:45:13 -0700

    AndroidPatternLock
    Puzzle to print all possible android pattern combinations of size 5. No
    repetetions.

---
+++ src/Graphs/AndroidPatternLock.java
@@ -0,0 +1,105 @@
+package Graphs;
+
+import java.io.PrintWriter;
+
+import Trees.Tree;
+
+/** List of all combinations for the Android pattern lock */
+public class AndroidPatternLock extends Tree<Byte> {
+
+	public static final byte LENGTH_MIN = 5;
+	public static final byte LENGTH_MAX = 5;
+	public static final byte NUMBER_MIN = 1;
+	public static final byte NUMBER_MAX = 9;
+	
+	public AndroidPatternLock(Byte b, AndroidPatternLock t) {
+		super(b, t);
+	}
+	
+	private boolean isMovePossible(byte to) {
+		return !hasAncestor(to) && (getNumberGroup(value) != getNumberGroup(to) || hasAncestor(getMiddle(value, to)));
+	}
+	
+	private Byte getMiddle(byte from, byte to) {
+		if (from != to && getNumberGroup(from) == getNumberGroup(to)) {
+			return (byte) ((from+to) / 2);
+		}
+		else {
+			return null;
+		}
+	}
+	
+	private static byte getNumberGroup(byte from) {
+		if (from == 0) {
+			return 0;
+		}
+		else if (from == 1 || from == 3 || from == 7 || from == 9) {
+			return 1;
+		}
+		else if (from == 2 || from == 8) {
+			return 2;
+		}
+		else if (from == 4 || from == 6) {
+			return 3;
+		}
+		else {
+			return 4;
+		}
+	}
+	
+	public void build() {
+		for (byte to = NUMBER_MIN; to <= NUMBER_MAX; to++) {
+			if (isMovePossible(to)) {
+				AndroidPatternLock child = new AndroidPatternLock(to, this);
+				if (child.level < LENGTH_MAX) {
+					child.build();
+				}
+			}
+		}
+	}
+	
+	public String toString(boolean pathOnly) {
+		StringBuilder out = new StringBuilder();
+		if (level >= LENGTH_MIN || pathOnly) {
+			if (parent != null) {
+				out.append(((AndroidPatternLock) parent).toString(true));
+			}
+			if (value >= NUMBER_MIN && value <= NUMBER_MAX) {
+				out.append(value.toString());
+			}
+		}
+		
+		if (!pathOnly && children.size() > 0) {
+			for (Tree<Byte> child : children) {
+				if (out.length() > 0) {
+					out.append("\n");
+				}
+				out.append(child.toString());
+			}
+		}
+		return out.toString();
+	}
+	
+	@Override
+	public String toString() {
+		return toString(false);
+	}
+	
+	public static void main(String[] args) {
+		AndroidPatternLock AndroidPatternLock = new AndroidPatternLock((byte) 0, null);
+		AndroidPatternLock.build();
+		
+		try {
+			PrintWriter writer = new PrintWriter("OUTPUT.txt", "UTF-8");
+			for (Tree<Byte> tree : AndroidPatternLock.children) {
+				writer.println(tree.toString());
+			}
+			writer.close();
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		System.out.println("OUTPUT.txt created");
+	}
+
+}
